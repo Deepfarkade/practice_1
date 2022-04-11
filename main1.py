d5777025ef1f26976cba08c645143920be55d009
deep <farkadedeep1999@gmail.com>
2022-04-12 00:15:59 +0530

    Updated dynamic URL

--- main1.py
+++ main1.py
@@ -6,6 +6,9 @@ app = Flask(__name__) #constructor of the flask
 def base_route():
     return "Welcome to Praxis"
 
+@app.route("/my_name/<name>")
+def print_name(name):
+    return f"Happy Leaning {name} in Praxis BLR" 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
